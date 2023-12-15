a0ed24dfd5dd6f7395f39180153ac4e0f01406d9
Otonda1 <charlesotonda@gmail.com>
2023-12-15 15:45:14 +0300

    my jquery folder

---
+++ jsquery/8-scripts.js
@@ -0,0 +1,9 @@
+
+const movies = $('UL#list_movies');
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json',
+  (data) => {
+    const films = data.results;
+    films.forEach(film => {
+      movies.append('<li>' + film.title + '</li>');
+    });
+  });
